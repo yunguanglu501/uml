d4ce71e80bd4d6ebe756e10e37f027e801a172f4
wcy <wcy@qq.com>
2022-09-29 10:53:26 +0800

    Initial commit

---
+++ src/main/java/com/example/uml/UmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uml/UmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
